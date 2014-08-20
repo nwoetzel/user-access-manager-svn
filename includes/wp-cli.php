b1021d0fcf44e33b9c32c7ef0e56ad613e967c23
woetzens <nils.woetzel@h-its.org>
2014-08-20 16:19:49 +0200

    implemented add function, to add a group on the commandline

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -15,17 +15,17 @@ if (!defined('ABSPATH')) {
  * @subpackage commands/community
  * @maintainer nwoetzel
  */
-class Groups_Command extends WP_CLI_Command {
+class Groups_Command extends \WP_CLI\CommandWithDBObject {
 
-    protected $obj_type = 'UamUserGroup';
+    protected $obj_type = 'uam_accessgroup';
     protected $obj_fields = array(
-            'id',
-            TXT_UAM_NAME,
-            TXT_UAM_DESCRIPTION,
-            TXT_UAM_READ_ACCESS,
-            TXT_UAM_WRITE_ACCESS,
-            TXT_UAM_GROUP_ROLE,
-            TXT_UAM_IP_RANGE,
+            'ID',
+            'groupname',
+            'groupdesc',
+            'read_access',
+            'write_access',
+            'roles',
+            'ip_range',
     );
 
     /**
@@ -54,13 +54,13 @@ class Groups_Command extends WP_CLI_Command {
 
         foreach ($aUamUserGroups as $oUamUserGroup) {
             $group = array();
-            $group[ 'id'] = $oUamUserGroup->getId();
-            $group[ TXT_UAM_NAME] = $oUamUserGroup->getGroupName();
-   			$group[ TXT_UAM_DESCRIPTION] = $oUamUserGroup->getGroupDesc();
-            $group[ TXT_UAM_READ_ACCESS] = $oUamUserGroup->getReadAccess();
-            $group[ TXT_UAM_WRITE_ACCESS] = $oUamUserGroup->getWriteAccess();
-            $group[ TXT_UAM_GROUP_ROLE] = implode( ",", array_keys( $oUamUserGroup->getObjectsFromType('role')));
-            $group[ TXT_UAM_IP_RANGE] = $oUamUserGroup->getIpRange() == null ? "" : $oUamUserGroup->getIpRange();
+            $group[ 'ID'          ] = $oUamUserGroup->getId();
+            $group[ 'groupname'   ] = $oUamUserGroup->getGroupName();
+   			$group[ 'groupdesc'   ] = $oUamUserGroup->getGroupDesc();
+            $group[ 'read_access' ] = $oUamUserGroup->getReadAccess();
+            $group[ 'write_access'] = $oUamUserGroup->getWriteAccess();
+            $group[ 'roles'       ] = implode( ",", array_keys( $oUamUserGroup->getObjectsFromType('role')));
+            $group[ 'ip_range'    ] = $oUamUserGroup->getIpRange() == null ? "" : $oUamUserGroup->getIpRange();
 
             // add current group
             $groups[] = $group;
@@ -101,6 +101,93 @@ class Groups_Command extends WP_CLI_Command {
         return new \WP_CLI\Formatter( $assoc_args, $this->obj_fields, $this->obj_type );
     }
 
+    /**
+     * add group
+     *
+     * ## OPTIONS
+     * <groupname>
+     * : the name of the new group
+     *
+     * [--porcelain]
+	 * : Output just the new post id.
+	 *
+	 * [--roles=<list>]
+	 * : comma seperated list of group associated roles
+	 *
+	 * [--<field>=<value>]
+	 * : Associative args for new UamUserGroup object
+	 * allowed fields and values are: groupdesc="", read_access={group,all*}, write_access={group,all*}, ip_range="192.168.0.1-192.168.0.10;192.168.0.20-192.168.0.30"
+	 * *=default
+	 *
+     * ## EXAMPLES
+     *
+     * wp groups add fighters --read_access=all
+     *
+     */
+    public function add( $_, $assoc_args) {
+        global $oUserAccessManager;
+
+        $porcelain = isset( $assoc_args[ "porcelain"]);
+
+        $groupname      = $_[0];
+
+        $aUamUserGroups = $oUserAccessManager->getAccessHandler()->getUserGroups();
+
+        foreach ($aUamUserGroups as $oUamUserGroup) {
+            if( $oUamUserGroup->getGroupName() == $groupname) {
+                WP_CLI::error( "group with the same name already exists: " . $oUamUserGroup->getId());
+            }
+        }
+
+        $groupdesc      = $assoc_args[ 'groupdesc'];
+        $read_access    = $assoc_args[ 'read_access'];
+        $write_access   = $assoc_args[ 'write_access'];
+        $ip_range       = $assoc_args[ 'ip_range'];
+
+        $oUamUserGroup = new UamUserGroup($oUserAccessManager->getAccessHandler(), null);
+
+        if( $read_access == null || ( $read_access != 'all' || $read_access != 'group')) {
+            if( !$porcelain) {
+                WP_CLI::line( "setting read_access to 'group'");
+            }
+            $read_access = 'group';
+        }
+
+        if( $write_access == null || ( $write_access != 'all' || $write_access != 'group')) {
+            if( !$porcelain) {
+                WP_CLI::line( "setting write_access to 'group'");
+            }
+            $write_access = 'group';
+        }
+
+        $oUamUserGroup->setGroupName(   $groupname);
+        $oUamUserGroup->setGroupDesc(   $groupdesc);
+        $oUamUserGroup->setReadAccess(  $read_access);
+        $oUamUserGroup->setWriteAccess( $write_access);
+        $oUamUserGroup->setIpRange(     $ip_range);
+
+        // add roles
+        if( isset( $assoc_args[ 'roles'])) {
+            $roles = explode( ",", $assoc_args[ 'roles']);
+
+            $oUamUserGroup->unsetObjects('role', true);
+
+            foreach ($roles as $role) {
+                $oUamUserGroup->addObject('role', $role);
+            }
+        }
+
+        $oUamUserGroup->save();
+
+        $oUserAccessManager->getAccessHandler()->addUserGroup($oUamUserGroup);
+
+        if( $porcelain) {
+            WP_CLI::line( $oUamUserGroup->getId());
+        }
+        else {
+            WP_CLI::success( "added new group " . $groupname . " with id " . $oUamUserGroup->getId());
+        }
+    }
 }
 
 // add the command
