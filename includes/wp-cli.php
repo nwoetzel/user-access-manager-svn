6a9ce6558fa8f6683082acdbd29b85910bb30e69
woetzens <nils.woetzel@h-its.org>
2014-08-20 14:09:43 +0200

    added wp-cli.php with GroupsCommand class that implements a list function, listing all groups

---
+++ includes/wp-cli.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the user-access-manager, a plugin for WordPress.
+ *
+ */
+
+if (!defined('ABSPATH')) {
+    die();
+}
+
+/**
+ * manage groups in the user-access-manager plugin
+ *
+ * @package wp-cli
+ * @subpackage commands/community
+ * @maintainer nwoetzel
+ */
+class Groups_Command extends WP_CLI_Command {
+
+    protected $obj_type = 'UamUserGroup';
+    protected $obj_fields = array(
+            'id',
+            TXT_UAM_NAME,
+            TXT_UAM_DESCRIPTION,
+            TXT_UAM_READ_ACCESS,
+            TXT_UAM_WRITE_ACCESS,
+            TXT_UAM_GROUP_ROLE,
+            TXT_UAM_IP_RANGE,
+    );
+
+    /**
+     * list groups
+     *
+     * ## OPTIONS
+     *
+	 * [--format=<format>]
+	 * : Accepted values: table, csv, json, count, ids. Default: table
+	 *
+     * ## EXAMPLES
+     *
+     * wp groups list
+     *
+     * @subcommand list
+     */
+    public function list_( $_, $assoc_args) {
+        global $oUserAccessManager;
+        $aUamUserGroups = $oUserAccessManager->getAccessHandler()->getUserGroups();
+
+        if (!isset($aUamUserGroups)) {
+            WP_CLI:error( "no groups defined yet!");
+        }
+
+        $groups = array();
+
+        foreach ($aUamUserGroups as $oUamUserGroup) {
+            $group = array();
+            $group[ 'id'] = $oUamUserGroup->getId();
+            $group[ TXT_UAM_NAME] = $oUamUserGroup->getGroupName();
+   			$group[ TXT_UAM_DESCRIPTION] = $oUamUserGroup->getGroupDesc();
+            $group[ TXT_UAM_READ_ACCESS] = $oUamUserGroup->getReadAccess();
+            $group[ TXT_UAM_WRITE_ACCESS] = $oUamUserGroup->getWriteAccess();
+            $group[ TXT_UAM_GROUP_ROLE] = implode( ",", array_keys( $oUamUserGroup->getObjectsFromType('role')));
+            $group[ TXT_UAM_IP_RANGE] = $oUamUserGroup->getIpRange() == null ? "" : $oUamUserGroup->getIpRange();
+
+            // add current group
+            $groups[] = $group;
+        }
+
+        $formatter = $this->get_formatter( $assoc_args );
+        $formatter->display_items( $groups );
+    }
+
+    protected function get_formatter( &$assoc_args ) {
+        return new \WP_CLI\Formatter( $assoc_args, $this->obj_fields, $this->obj_type );
+    }
+
+}
+
+// add the command
+WP_CLI::add_command( 'uam groups', 'Groups_Command' );
