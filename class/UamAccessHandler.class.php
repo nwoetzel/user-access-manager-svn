09622ad47afad6901991aac92d14d57b37b8d7d3
woetzens <nils.woetzel@h-its.org>
2014-09-09 19:03:47 +0200

    corrected handling of custom post types, by hooking to registered_post_types and adding them to the validObjects handling, when they are queryable the previous way did assume, that "get_post_types" returns all post_types, but since the UamAccessHandler is constructed long before the custom post types are registered, it could not add them properly

--- class/UamAccessHandler.class.php
+++ class/UamAccessHandler.class.php
@@ -60,17 +60,34 @@ class UamAccessHandler
     {
         $this->_oUserAccessManager = $oUserAccessManager;
 
-        $aPostTypes = get_post_types(array(), 'objects');
-
-        foreach ($aPostTypes as $oPostType) {
-            if ($oPostType->publicly_queryable) {
-                $this->_aPostableTypes[] = $oPostType->name;
-            }
-        }
+        $this->_aPostableTypes = array_merge( $this->_aPostableTypes, get_post_types(array('publicly_queryable' => true), 'names'));
+        $this->_aPostableTypes = array_unique( $this->_aPostableTypes);
 
         $this->_aPostableTypesMap = array_flip($this->_aPostableTypes);
 
         $this->_aObjectTypes = array_merge($this->_aPostableTypes, $this->_aObjectTypes);
+        add_action( 'registered_post_type', array( &$this, 'registered_post_type'), 10, 2);
+    }
+
+    /**
+     * used for adding custom post types using the registered_post_type hook
+     * @see http://wordpress.org/support/topic/modifying-post-type-using-the-registered_post_type-hook
+     *
+     * @param $post_type the string for the new post_type
+     * @param $args the array of arguments used to create the post_type
+     *
+     */
+    public function registered_post_type( $post_type, $args)
+    {
+        if( $args->publicly_queryable) {
+            $this->_aPostableTypes[] = $args->name;
+            $this->_aPostableTypes = array_unique( $this->_aPostableTypes);
+            $this->_aPostableTypesMap = array_flip($this->_aPostableTypes);
+            $this->_aObjectTypes = array_merge($this->_aPostableTypes, $this->_aObjectTypes);
+            $this->_aAllObjectTypes = null;
+            $this->_aAllObjectTypesMap = null;
+            $this->_aValidObjectTypes = null;
+        }
     }
 
     /**
